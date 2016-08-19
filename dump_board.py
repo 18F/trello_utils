6e818567bdf29683165de9c3f74c0ec05090fc44
thecapacity <william.huie@gsa.gov>
2016-08-19 17:42:02 -0400

    Removed Dead Code
    Removed option to write to a file or erase the board

--- dump_board.py
+++ dump_board.py
@@ -31,7 +31,6 @@ def cli():
 def dump_list_cards(in_list):
     list_cards = trello.lists.get(in_list['id'], cards="all")['cards']
 
-    # http://click.pocoo.org/5/utils/#printing-to-stdout
     for c in list_cards:
 
         print "\t* ###", c['name'].encode('utf8'), "###"
@@ -51,28 +50,12 @@ def dump_list_cards(in_list):
             print "\t`"
 
 @cli.command()
-@click.option('--name', default="board_dump.yaml", help="Filename to dump contents if not default")
 def dump_board(name):
-    #click.echo(click.format_filename(name))
-
-    #test_file = click.open_file('test.txt', 'w')
-    #with click.open_file(filename, 'w') as f:
-    #        f.write('Hello World!\n')
-
     board_lists = trello.boards.get_list( config['BOARD_ID'] )
 
     for l in board_lists:
         print "##", l['name'].encode('utf8')
         dump_list_cards(l)
-
-def abort_if_false(ctx, param, value):
-    if not value: ctx.abort()
-
-@cli.command()
-@click.option('--erase', is_flag=True, default=False, callback=abort_if_false,
-              expose_value=False, prompt="Are you sure you want to delete everything?")
-def erase_board():
-    click.echo("erasing the board")
 
 config, trello = setup_trello()
 if __name__ == "__main__":
